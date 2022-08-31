d526be897559b40d49e636909854138681fde644
RAFAEL PEREZ (GEDI-ISD-OOCLL/MNL) <PEREZRA@oocl.com>
2022-08-31 17:56:47 +0800

    commit streamMap

---
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -0,0 +1,25 @@
+
+package com.thoughtworks.collection;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+
+    public List<Integer> filterEven(List<Integer> numbers) {
+
+        return numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> filterEndWithS(List<String> words) {
+        return null;
+    }
+
+    public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
+        return null;
+    }
+
+
+}
